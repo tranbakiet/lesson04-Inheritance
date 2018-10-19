e464bed1c24c8385d3e4a774c04ad33cccb066c8
Kiet <206love193@gmail.com>
2018-10-19 23:10:39 +0700

    Finish abstract implement

--- src/kiet/dev/Square.java
+++ src/kiet/dev/Square.java
@@ -1,5 +1,16 @@
 package kiet.dev;
 
 public class Square extends Shape {
-
+	private double chieudai;
+	private double chieurong;
+	
+	public Square(double chieudai, double chieurong) {
+		this.chieudai= chieudai;
+		this.chieurong= chieurong;
+	}
+	@Override
+	public double Dientich() {
+		// TODO Auto-generated method stub
+		return chieudai*chieurong;
+	}
 }
